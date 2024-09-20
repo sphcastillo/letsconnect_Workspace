43d0d7f807d24e732fc4a703e1fa3c2b9a643211
sphcastillo <sphcastillo@gmail.com>
2024-09-19 17:32:35 -0700

    ChatToDocument ~ added but not successfully yet

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -12,6 +12,7 @@ import '@blocknote/core/fonts/inter.css';
 import '@blocknote/shadcn/style.css';
 import stringToColor from "@/lib/stringToColor";
 import TranslateDocument from "./TranslateDocument";
+import ChatToDocument from "./ChatToDocument";
 
 type EditorProps = {
     darkMode: boolean;
@@ -76,7 +77,7 @@ function Editor() {
         <div className="max-w-6xl mx-auto">
             <div className="flex items-center gap-2 justify-end mb-10">
                 <TranslateDocument doc={doc}/>
-                {/* ChatToDocument AI */}
+                <ChatToDocument doc={doc}/>
 
                 {/* Dark Mode */}
                 <Button className={style} onClick={() => setDarkMode(!darkMode)}>
