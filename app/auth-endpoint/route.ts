d91517818336ffd009a5cc14d198af536226f134
sphcastillo <sphcastillo@gmail.com>
2024-09-20 10:55:14 -0700

    Shooting to fix error on auth-endpoint

--- app/auth-endpoint/route.ts
+++ app/auth-endpoint/route.ts
@@ -1,48 +1,38 @@
-import { NextRequest, NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs/server";
-import liveblocks from "@/lib/liveblocks";
 import { adminDb } from "@/firebase-admin";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import liveblocks from "@/lib/liveblocks";
 
 export async function POST(req: NextRequest) {
-  // Get authentication details
+  auth().protect(); // Ensure the user is authenticated
+
   const { userId, sessionClaims } = await auth();
+  const { room } = await req.json();
 
-  if (!userId || !sessionClaims) {
-    return NextResponse.json({ message: "User not authenticated" }, { status: 401 });
-  }
+  const session = liveblocks.prepareSession(sessionClaims?.email!, {
+    userInfo: {
+      name: sessionClaims?.fullName!,
+      email: sessionClaims?.email!,
+      avatar: sessionClaims?.image!,
+    },
+  });
 
-  try {
-    const { room } = await req.json();
+  const usersInRoom = await adminDb
+    .collectionGroup("rooms")
+    .where("userId", "==", sessionClaims?.email)
+    .get();
 
-    // Prepare liveblocks session
-    const session = liveblocks.prepareSession(sessionClaims.email, {
-      userInfo: {
-        name: sessionClaims.fullName || "Anonymous",
-        email: sessionClaims.email,
-        avatar: sessionClaims.image || "",
-      },
-    });
+  const userInRoom = usersInRoom.docs.find((doc) => doc.id === room);
 
-    // Check if the user is in the specified room
-    const usersInRoom = await adminDb
-      .collectionGroup('rooms')
-      .where("userId", "==", sessionClaims.email)
-      .get();
+  if (userInRoom?.exists) {
+    session.allow(room, session.FULL_ACCESS);
+    const { body, status } = await session.authorize();
 
-    const userInRoom = usersInRoom.docs.find((doc) => doc.id === room);
-
-    if (userInRoom?.exists) {
-      // Authorize access
-      session.allow(room, session.FULL_ACCESS);
-      const { body, status } = await session.authorize();
-
-      console.log("You are authorized to access this room");
-      return new NextResponse(body, { status });
-    } else {
-      return NextResponse.json({ message: "You are not in this room" }, { status: 403 });
-    }
-  } catch (error) {
-    console.error("Error processing request", error);
-    return NextResponse.json({ message: "Error processing request" }, { status: 500 });
+    return new Response(body, { status });
+  } else {
+    return NextResponse.json(
+      { message: "You are not in this room" },
+      { status: 403 }
+    );
   }
 }
